cf492433dd03ba2b5ac3bea4c7370cb72229d9ea
mckunkel <mkunkel@jlab.org>
2017-11-03 10:08:35 +0100

    added content for fifth class

--- CANAJAVA/src/main/java/fourthclass/UsingFinally.java
+++ CANAJAVA/src/main/java/fourthclass/UsingFinally.java
@@ -1,8 +1,11 @@
 package fourthclass;
 
+import org.jlab.groot.data.H2F;
+
 public class UsingFinally {
 	public static void main(String[] args) {
 		System.out.println("Is -2 degrees cold? " + isCold(-2));
+		H2F aH2f = new H2F("aname", 3, 0, 4, 3, 0, 4);
 	}
 
 	private static boolean isCold(int i) {
