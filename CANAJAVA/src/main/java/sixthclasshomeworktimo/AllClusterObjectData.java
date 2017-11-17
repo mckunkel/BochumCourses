e30aa07c091e2384aa4f11923d5d0d3e306a3108
timo <timo.schorlepp@rub.de>
2017-11-17 09:49:23 +0100

    Add binding energy plot and Hubble constant determination

---
+++ CANAJAVA/src/main/java/sixthclasshomeworktimo/AllClusterObjectData.java
@@ -0,0 +1,47 @@
+package sixthclasshomeworktimo;
+
+public class AllClusterObjectData {
+
+}
+
+class Virgo extends Cluster {
+	public Virgo() {
+		this.distance = 15;
+		this.velocity = 1200;
+	}
+}
+
+class UrsaMajor extends Cluster {
+	public UrsaMajor() {
+		this.distance = 190;
+		this.velocity = 15400;
+	}
+}
+
+class CoronaBorealis extends Cluster {
+	public CoronaBorealis() {
+		this.distance = 280;
+		this.velocity = 22000;
+	}
+}
+
+class Bootes extends Cluster {
+	public Bootes() {
+		this.distance = 490;
+		this.velocity = 39400;
+	}
+}
+
+class Hydra extends Cluster {
+	public Hydra() {
+		this.distance = 760;
+		this.velocity = 60600;
+	}
+}
+
+class Quasar extends Cluster {
+	public Quasar() {
+		this.distance = 4035;
+		this.velocity = 280000;
+	}
+}
---
+++ CANAJAVA/src/main/java/sixthclasshomeworktimo/Cluster.java
@@ -0,0 +1,16 @@
+package sixthclasshomeworktimo;
+
+public class Cluster {
+
+	protected double distance; // Mpc
+	protected double velocity;// km/s
+
+	public double getDistance() {
+		return this.distance;
+	}
+
+	public double getVelocity() {
+		return this.velocity;
+	}
+
+}
