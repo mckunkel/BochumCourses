dad824523a74917fea658e8612df87ac1d8ea271
timo <timo.schorlepp@rub.de>
2017-12-06 15:14:58 +0100

    Add final pressure integration GUI

--- CANAJAVA/src/main/java/eigthclasshomeworktimo/MainFrame.java
+++ CANAJAVA/src/main/java/eigthclasshomeworktimo/MainFrame.java
@@ -1,9 +1,14 @@
 package eigthclasshomeworktimo;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -11,6 +16,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import eigthclass.GUI.last.PanelConstraints;
 
 public class MainFrame extends JFrame implements IterationEventListener {
 
@@ -75,6 +83,18 @@ public class MainFrame extends JFrame implements IterationEventListener {
 		this.densityLabel.setText(AppConstants.DENSITY_LABEL_TXT);
 		this.stepLabel.setText(AppConstants.STEP_LABEL_TXT);
 		this.methodLabel.setText(AppConstants.METHOD_LABEL_TXT);
+		this.currentPressureLabel.setText("p = 0");
+		this.currentRadiusLabel.setText("r = R_0");
+
+		// set fixed size for numerical output
+		// this.currentPressureLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+		Dimension d = this.currentPressureLabel.getPreferredSize();
+		this.currentPressureLabel.setPreferredSize(new Dimension(d.width + 200, d.height));
+
+		this.radius0Text.setText("1");
+		this.radius1Text.setText("0.8");
+		this.densityText.setText("1");
+		this.stepText.setText("10000");
 
 		this.methodNames = new String[2];
 		this.methodNames[0] = "Euler";
@@ -91,17 +111,29 @@ public class MainFrame extends JFrame implements IterationEventListener {
 	private void buttonClicked(ActionEvent event) {
 		changeStatus(Color.red, "Working...");
 
-		double R0 = Double.parseDouble(this.radius0Text.getText());
-		double R = Double.parseDouble(this.radius1Text.getText());
-		double rho = Double.parseDouble(this.densityText.getText());
-		int steps = Integer.parseInt(this.stepText.getText());
-		String methodName = (String) this.methodBox.getSelectedItem();
+		try {
 
-		final IntegrationWorker integrationWorker = new IntegrationWorker(rho, R0, R, steps, methodName);
-		integrationWorker.addListener(this);
-		integrationWorker.addListener(new PrintPressure());
-		integrationWorker.iterateToR();
-		integrationWorker.removeListener(this);
+			double R0 = Double.parseDouble(this.radius0Text.getText());
+			double R = Double.parseDouble(this.radius1Text.getText());
+			double rho = Double.parseDouble(this.densityText.getText());
+			int steps = Integer.parseInt(this.stepText.getText());
+			String methodName = (String) this.methodBox.getSelectedItem();
+
+			final IntegrationWorker integrationWorker = new IntegrationWorker(rho, R0, R, steps, methodName);
+			final IterationEventListener listener = this;
+			Thread thread = new Thread() {
+				public void run() {
+					integrationWorker.addListener(listener);
+					integrationWorker.addListener(new PrintPressure());
+					integrationWorker.iterateToR();
+					integrationWorker.removeListener(listener);
+				}
+			};
+			thread.start();
+		} catch (NumberFormatException e) {
+			changeStatus(Color.red, "Input missing");
+			return;
+		}
 	}
 
 	private void changeStatus(Color color, String string) {
@@ -110,6 +142,62 @@ public class MainFrame extends JFrame implements IterationEventListener {
 	}
 
 	private void constructLayout() {
+		setLayout(new BorderLayout());
+		constructJPanel();
+		add(getInputPanel(), BorderLayout.NORTH);
+		add(panel, BorderLayout.CENTER);
+	}
+
+	private JPanel getInputPanel() {
+		JPanel fileInfoPanel = new JPanel();
+		Border spaceBorder = BorderFactory.createEmptyBorder(AppConstants.BORDER_SPACING, AppConstants.BORDER_SPACING,
+				AppConstants.BORDER_SPACING, AppConstants.BORDER_SPACING);
+		Border titleBorder = BorderFactory.createTitledBorder(AppConstants.FILE_FORM_SELECT);
+		fileInfoPanel.setBorder(BorderFactory.createCompoundBorder(spaceBorder, titleBorder));
+		fileInfoPanel.setLayout(new GridBagLayout());
+		Insets rightPadding = new Insets(0, 0, 0, 0);
+		Insets noPadding = new Insets(0, 0, 0, 0);
+
+		PanelConstraints.addComponent(fileInfoPanel, radius0Label, 0, 0, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, rightPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, radius0Text, 0, 1, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, rightPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, radius1Label, 1, 0, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, noPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, radius1Text, 1, 1, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, noPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, densityLabel, 0, 2, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, rightPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, densityText, 0, 3, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, rightPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, stepLabel, 1, 2, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, noPadding, 0, 0);
+		PanelConstraints.addComponent(fileInfoPanel, stepText, 1, 3, 1, 1, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, noPadding, 0, 0);
+
+		return fileInfoPanel;
+	}
+
+	private void constructJPanel() {
+
+		this.panel.add(methodLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(methodBox, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.LINE_START,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(runLabel, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(runButton, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(currentRadiusLabel, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(currentPressureLabel, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(progressbar, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(progressLabel, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		this.panel.add(statusLabel, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 
 	}
 
@@ -134,7 +222,7 @@ public class MainFrame extends JFrame implements IterationEventListener {
 		progressLabel.setText((String) (new Integer(progressbar.getValue())).toString() + "%");
 
 		currentRadiusLabel.setText((String) "r = " + (new Double(event.getR())).toString() + " m");
-		currentPressureLabel.setText((String) "r = " + (new Double(event.getP())).toString() + " m");
+		currentPressureLabel.setText((String) "p = " + (new Double(event.getP())).toString() + " Pa");
 
 		if (event.isFinished()) {
 			changeStatus(Color.black, "Finished, Results saved to file");
--- CANAJAVA/src/main/java/eigthclasshomeworktimo/PrintPressure.java
+++ CANAJAVA/src/main/java/eigthclasshomeworktimo/PrintPressure.java
@@ -11,19 +11,21 @@ public class PrintPressure implements IterationEventListener {
 		Calendar now = Calendar.getInstance();
 
 		try {
-			FileWriter fileWriter = new FileWriter("PressureData.dat", true);
 			if (event.isStarted()) {
+				FileWriter fileWriter = new FileWriter("PressureData.dat", false);
 				fileWriter.write("Started at " + now.getTime() + "\n");
 				fileWriter.write(
 						"Step " + event.getStep() + ": r = " + event.getR() + " m, p = " + event.getP() + "Pa \n");
+				fileWriter.close();
 			} else {
+				FileWriter fileWriter = new FileWriter("PressureData.dat", true);
 				if (event.isFinished()) {
 					fileWriter.write("Finished at " + now.getTime() + "\n");
 				} else
 					fileWriter.write(
 							"Step " + event.getStep() + ": r = " + event.getR() + " m, p = " + event.getP() + "Pa \n");
+				fileWriter.close();
 			}
-			fileWriter.close();
 		} catch (Exception e) {
 			System.err.println("Mistakes were made" + e);
 		}
